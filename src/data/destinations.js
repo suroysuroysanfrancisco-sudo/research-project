e69b6adbff656e534aad1d562a96114b4a9bb856
carlou-bit <carlouluchavez.contact@gmail.com>
2025-12-01 12:37:22 +0800

    Update destinations.js

--- src/data/destinations.js
+++ src/data/destinations.js
@@ -14,7 +14,8 @@ export const destinations = [
       "Pristine white sand beach with crystal-clear turquoise waters, perfect for swimming, snorkeling, and beach activities.",
     image: santiagoBay,
     address: "Santiago, San Francisco, Camotes Island, Cebu",
-    mapEmbed: "YOUR_MAP_EMBED_URL_HERE",
+    mapEmbed: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5876.156321072152!2d124.29956416384313!3d10.58981418513826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33a9cda24b9ccbc3%3A0xb6a7048b13dd7429!2sSantiago%20Bay%20Beach!5e0!3m2!1sen!2sph!4v1764561283711!5m2!1sen!2sph",
+    
   },
   {
     id: "timubo-cave",
